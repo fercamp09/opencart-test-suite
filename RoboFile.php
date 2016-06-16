689b8d362e752922bdedb024fafc6a0107f9ac87
Fernando Campana <fercamp09@gmail.com>
2016-06-15 23:05:42 -0500

    Update composer

--- RoboFile.php
+++ RoboFile.php
@@ -21,7 +21,7 @@ class RoboFile extends \Robo\Tasks
     {
         $this->taskDeleteDir('www')->run();
         $this->taskFileSystemStack()
-            ->mirror('vendor/leoeras/Expigo', 'www')
+            ->mirror('vendor/leoeras/opencart', 'www')
             ->copy('src/test-config.php', 'www/system/config/test-config.php')
             ->copy('src/test-catalog-startup.php','www/catalog/controller/startup/test_startup.php')
             ->chmod('www', 0777, 0000, true)
