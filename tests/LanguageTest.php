9afec38af0ce29a5494978574d2ba310adf6a9d3
Fernando Campana <fercamp09@gmail.com>
2016-06-21 00:57:50 -0500

    Edited xml

---
+++ tests/LanguageTest.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/../src/OpenCartTest.php';
+
+class LanguageTest extends OpenCartTest
+{
+
+    public function testEnglishLanguage()
+    {
+        $model = $this->loadModel("localisation/language");
+        $language = $model->getLanguage(1);
+        $this->assertEquals('English', $language['name']);
+        $this->assertEquals('en-en', $language['code']);
+    }
+
+}
